63bcd935ea80f20d3dad3ae45868d5530e7adc30
Andreas Bok Andersen <bok.chan@gmail.com>
2018-09-05 09:51:09 +0200

    Fix syntax error

--- scripts/bokchan.shortcuts.util.js
+++ scripts/bokchan.shortcuts.util.js
@@ -9,7 +9,7 @@ function create_shortcut_help(shortcuts) {
     "use strict";
     var help_content_style = '<style type="text/css">#modal_header{font-weight: bold;}.modal_divider{width: 20px;}</style>';
 
-    var help_content = help_content_style.concat('<div><tableid=modal_table><theadid=modal_header><tr><td>shortcut</td><tdclass="modal_divider"></td><td>description</td></tr></thead>');
+    var help_content = help_content_style.concat('<div><table id=modal_table><thead id=modal_header><tr><td>shortcut</td><td class="modal_divider"></td><td>description</td></tr></thead>');
         shortcuts.forEach(function(e)
         {
 
