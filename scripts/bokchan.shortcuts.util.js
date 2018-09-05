17ffb917ee76cbb0f57ba3d80545c91cb912f69a
Andreas Bok Andersen <bok.chan@gmail.com>
2018-09-05 09:24:54 +0200

    Some fixes to util script

--- scripts/bokchan.shortcuts.util.js
+++ scripts/bokchan.shortcuts.util.js
@@ -1,15 +1,3 @@
-var help_content_style = `
-<style type="text/css">
-#modal_header
-{
-font-weight: bold;
-}
-.modal_divider
-{
-width: 20px;
-}
-</style>
-`
 /**
 * @brief Create help modal for a list of shortcuts
 * @details
@@ -17,31 +5,21 @@ width: 20px;
 * @param shortcuts array of shortcuts
 * @return help content formatted as html
 */
-function create_shortcut_help ( shortcuts )
-{
-console.log(shortcuts);
-var help_content = `
-<div>
-    <table id=modal_table>
-        <thead id=modal_header><tr>
-            <td>shortcut</td>
-            <td class="modal_divider"></td>
-            <td>description</td>
-        </tr></thead>
-        `;
-        shortcuts.forEach(function(e) {
-        help_content = help_content.concat(`
-        <tr>
-            <td><code>` + e[1] + `</code></td>
-            <td class="modal_divider"></td>
-            <td>` + e[0] + `</td>
-        </tr>
-        `);
+function create_shortcut_help(shortcuts) {
+    "use strict";
+    var help_content_style = '<style type="text/css">#modal_header{font-weight: bold;}.modal_divider{width: 20px;}</style>';
+
+    var help_content = help_content_style.concat('<div><tableid=modal_table><theadid=modal_header><tr><td>shortcut</td><tdclass="modal_divider"></td><td>description</td></tr></thead>');
+        shortcuts.forEach(function(e)
+        {
+
+        help_content = help_content + '<tr><td><code>' + e[1] + '</code></td>' +
+            '<td class="modal_divider"></td>' +
+            '<td>' + e[0] + '</td>' +
+            '</tr>';
+
         });
-        help_content += `
-    </table>
-</div>
-`;
-console.log(help_content);
+        help_content += '</table></div>';
+
 return help_content;
-};
+}
