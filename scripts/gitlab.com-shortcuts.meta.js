63db405cb1b8a2d33b37389c9ef2842a996a2b44
Andreas Bok Andersen <bok.chan@gmail.com>
2019-12-07 20:04:01 +0100

    Bump version 0.0.7

--- scripts/gitlab.com-shortcuts.meta.js
+++ scripts/gitlab.com-shortcuts.meta.js
@@ -4,8 +4,8 @@
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @author       Andreas Bok Andersen
 // @description  Github custom shortcuts
-// @date         05.12.2019
-// @version      0.0.6
+// @date         07.12.2019
+// @version      0.0.7
 // @match        https://*.gitlab.com/*
 // @downloadURL  https://raw.githubusercontent.com/bokchan/tampermonkey-scripts/master/scripts/gitlab.com-shortcuts.user.js
 // @homepageURL  https://github.com/bokchan/tampermonkey-scripts
