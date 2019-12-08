4b16c5ade484b49d9b340f43e3409d89dab236fc
Andreas Bok Andersen <bok.chan@gmail.com>
2019-12-08 19:36:30 +0100

    Formatting fixes
    Make the code style adhere (more or less) to the jsdoc standard

--- scripts/bokchan.shortcuts.util.js
+++ scripts/bokchan.shortcuts.util.js
@@ -6,22 +6,20 @@
 * @return help content formatted as html
 */
 function create_shortcut_help(shortcuts) {
-    "use strict";
-    var help_content_style = '<style type="text/css">#modal_header{font-weight: bold;}.modal_divider{width: 20px;}</style>';
+  'use strict'
+  var helpContentStyle = '<style type="text/css">#modal_header{font-weight: bold;}.modal_divider{width: 20px;}</style>';
 
-    var help_content = help_content_style + '<div><table id="modal_table"><thead id="modal_header"><tr><td>shortcut</td><td class="modal_divider"></td><td>description</td></tr></thead>';
-        shortcuts.forEach(function(e)
-        {
+  var helpContent = helpContentStyle + '<div><table id="modal_table"><thead id="modal_header"><tr><td>shortcut</td><td class="modal_divider"></td><td>description</td></tr></thead>';
+  shortcuts.forEach(function (e) {
+    helpContent = helpContent + '<tr><td><code>' + e[1] + '</code></td>' +
+      '<td class="modal_divider"></td>' +
+      '<td>' + e[0] + '</td>' +
+      '</tr>'
+  })
 
-        help_content = help_content + '<tr><td><code>' + e[1] + '</code></td>' +
-            '<td class="modal_divider"></td>' +
-            '<td>' + e[0] + '</td>' +
-            '</tr>';
+  helpContent += '</table></div>'
 
-        });
-        help_content += '</table></div>';
-
-return help_content;
+  return helpContent
 }
 
 /**
@@ -31,16 +29,16 @@ return help_content;
  * @param      caretPos  The length of the current value of elem
  */
 function setCaretPosition(elem, caretPos) {
-    var range;
+  var range;
 
-    if (elem.createTextRange) {
-        range = elem.createTextRange();
-        range.move('character', caretPos);
-        range.select();
-    } else {
-        elem.focus();
-        if (elem.selectionStart !== undefined) {
-            elem.setSelectionRange(caretPos, caretPos);
-        }
+  if (elem.createTextRange) {
+    range = elem.createTextRange();
+    range.move('character', caretPos);
+    range.select()
+  } else {
+    elem.focus()
+    if (elem.selectionStart !== undefined) {
+      elem.setSelectionRange(caretPos, caretPos);
     }
+  }
 }
--- scripts/gitlab.com-shortcuts.user.js
+++ scripts/gitlab.com-shortcuts.user.js
@@ -16,102 +16,102 @@
 // @require      https://raw.githubusercontent.com/bokchan/tampermonkey-scripts/master/scripts/bokchan.shortcuts.util.js
 // ==/UserScript==
 
-const repo_regex = "(https?://gitlab.com/[^/]+/[^/]+)";
-const merge_request_regex = "(/merge_requests/[0-9]+)"
-const merge_request_list_regex = "/merge_requests(\\?scope=.+)?$"
-const repo_url = window.location.href.match(repo_regex);
+const repoRegex = '(https?://gitlab.com/[^/]+/[^/]+)';
+const mergeRequestRegex = '(/merge_requests/[0-9]+)'
+const mergeRequestListRegex = '/merge_requests(\\?scope=.+)?$'
+const repoUrl = window.location.href.match(repoRegex)
 
 /**
  * Util function to check if the redirect url is equal to the current url
  *
- * @param      {string}     url_suffix  The suffix to the current url to match
+ * @param      {string}     urlSuffix  The suffix to the current url to match
  *                          against
  * @return     {boolean}    True if same url, False otherwise.
  */
-function is_same_url(url_suffix) {
-  return window.location.href.match(repo_url[0] + url_suffix);
+function isSameUrl (urlSuffix) {
+  return window.location.href.match(repoUrl[0] + urlSuffix)
 }
 
-function match_url(url) {
-  return window.location.href.match(url);
+function currentUrlMatches (urlRegex) {
+  return window.location.href.match(urlRegex)
 }
 
-function click_element(selector) {
-  var el = document.querySelector(selector)
-  if (el) { el.click(); }
+function clickElement (selector) {
+  const el = document.querySelector(selector)
+  if (el) { el.click() }
 }
 
-function handle_shortcut(url, selector) {
-  if (match_url(url)) {
-    click_element(selector)
+function handleShortcut (urlRegex, selector) {
+  if (currentUrlMatches(urlRegex)) {
+    clickElement(selector)
   }
 }
 
-function handle_shortcut_multiple(urls_selectors) {
-  for (url_selector of urls_selectors) {
-    if (handle_shortcut(url_selector[0], url_selector[1])) break;
+function handleShortcutMultiple (urlElementSelectorPairs) {
+  for (const pair of urlElementSelectorPairs) {
+    if (handleShortcut(pair[0], pair[1])) break
   }
 }
 
 /**
  * @brief Switch to 'discussion' tab on merge requests
  */
-Mousetrap.bind("shift+1", function() {
-  handle_shortcut_multiple([
-    [merge_request_regex, ".notes-tab a"],
-    [merge_request_list_regex, "#state-opened"],
+Mousetrap.bind('shift+1', function () {
+  handleShortcutMultiple([
+    [mergeRequestRegex, '.notes-tab a'],
+    [mergeRequestListRegex, '#state-opened']
   ])
-});
+})
 
 /**
  * @brief Switch to the 'commits' tab on merge requests
  */
-Mousetrap.bind("shift+2", function() {
-  handle_shortcut_multiple([
-    [merge_request_regex, ".commits-tab a"],
-    [merge_request_list_regex, "#state-merged"],
-  ]);
-});
+Mousetrap.bind('shift+2', function () {
+  handleShortcutMultiple([
+    [mergeRequestRegex, '.commits-tab a'],
+    [mergeRequestListRegex, '#state-merged']
+  ])
+})
 
 /**
  * @brief Switch to the 'pipelines' tab on merge requests
  */
-Mousetrap.bind("shift+3", function() {
-  handle_shortcut_multiple([
-    [merge_request_regex, ".pipelines-tab a"],
-    [merge_request_list_regex, '#state-closed'],
-  ]);
-});
+Mousetrap.bind('shift+3', function () {
+  handleShortcutMultiple([
+    [mergeRequestRegex, '.pipelines-tab a'],
+    [mergeRequestListRegex, '#state-closed']
+  ])
+})
 
 /**
  * @brief Switch to 'changes' tab on merge requests
  */
-Mousetrap.bind("shift+4", function() {
-  handle_shortcut_multiple([
-    [merge_request_regex, ".diffs-tab a"],
-    [merge_request_list_regex, '#state-all'],
-  ]);
-});
+Mousetrap.bind('shift+4', function () {
+  handleShortcutMultiple([
+    [mergeRequestRegex, '.diffs-tab a'],
+    [mergeRequestListRegex, '#state-all']
+  ])
+})
 
 /**
  * @brief Sets the focus on the sub search input on the issues and pull requests
  *        listing page
  */
-Mousetrap.bind("f", function() {
-  var subnav_search = document.querySelector(
+Mousetrap.bind('f', function () {
+  var filteredSearch = document.querySelector(
     "input[class='form-control filtered-search']"
-  );
-  if (subnav_search != null) {
-    subnav_search.focus();
+  )
+  if (filteredSearch != null) {
+    filteredSearch.focus()
   }
-});
+})
 
 /**
  * @brief Changes to bulk edit mode
  */
 Mousetrap.bind('shift+e', function () {
-  handle_shortcut(repo_regex, '.js-bulk-update-toggle')
-});
+  handleShortcut(repoRegex, '.js-bulk-update-toggle')
+})
 
 /**
  * Navigate a list with shortcut keys
@@ -163,23 +163,22 @@ Mousetrap.bind(['j', 'k', 'o'],
 )
 
 var shortcuts = [
-  ["Discussion| Opened", "shift+1"],
-  ["Commits | Merged", "shift+2"],
-  ["Pipelines | Closed", "shift+3"],
-  ["Changes | All", "shift+4"],
+  ['Discussion| Opened', 'shift+1'],
+  ['Commits | Merged', 'shift+2'],
+  ['Pipelines | Closed', 'shift+3'],
+  ['Changes | All', 'shift+4'],
   ['Edit bulk', 'shift+e'],
-  ["Focus filtered search", "f"]
-];
   ['Focus filtered search', 'f'],
   ['Select next list item', 'j'],
   ['Select prev list item', 'k'],
   ['Open selected list item in new tab', 'o']
+]
 
-var help_content = create_shortcut_help(shortcuts);
+var helpContent = create_shortcut_help(shortcuts)
 
 /**
  * Show shortcuts
  */
-Mousetrap.bind("shift+h", function() {
-  picoModal(help_content).show();
-});
+Mousetrap.bind('shift+h', function () {
+  picoModal(helpContent).show()
+})
