f9aecb7b306aeafd1924737bd443eb85a9629d19
Andreas Bok Andersen <andreas.bokandersen@glooko.com>
2022-11-25 13:19:38 +0100

    Focus the squash editor

--- scripts/gitlab.com-shortcuts.user.js
+++ scripts/gitlab.com-shortcuts.user.js
@@ -4,7 +4,7 @@
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @author       Andreas Bok Andersen
 // @description  Github custom shortcuts
-// @version      0.0.30
+// @version      0.0.31
 // @match        https://*.gitlab.com/*
 // @downloadURL  https://raw.githubusercontent.com/bokchan/tampermonkey-scripts/master/scripts/gitlab.com-shortcuts.user.js
 // @homepageURL  https://github.com/bokchan/tampermonkey-scripts
@@ -274,6 +274,7 @@ Mousetrap.bind("ctrl+m", function (e) {
 
   if (currentUrlMatches(mergeRequestRegex) && edit_commit_message != null) {
     edit_commit_message.click();
+    document.querySelector("text_area#squash-message-edit").focus();
   }
 });
 
