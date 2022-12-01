ad4dfe36594d688a2f57b707313cb4ea0f062876
Andreas Bok Andersen <andreas.bokandersen@glooko.com>
2022-12-01 12:19:08 +0100

    Fix selector for edit merge commit message

--- scripts/gitlab.com-shortcuts.user.js
+++ scripts/gitlab.com-shortcuts.user.js
@@ -4,7 +4,7 @@
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @author       Andreas Bok Andersen
 // @description  Github custom shortcuts
-// @version      0.0.32
+// @version      0.0.33
 // @match        https://*.gitlab.com/*
 // @downloadURL  https://raw.githubusercontent.com/bokchan/tampermonkey-scripts/master/scripts/gitlab.com-shortcuts.user.js
 // @homepageURL  https://github.com/bokchan/tampermonkey-scripts
@@ -274,7 +274,7 @@ Mousetrap.bind("ctrl+m", function (e) {
 
   if (currentUrlMatches(mergeRequestRegex) && edit_commit_message != null) {
     edit_commit_message.click();
-    document.querySelector("text_area#squash-message-edit").focus();
+    document.querySelector("#squash-message-edit").focus();
   }
 });
 
